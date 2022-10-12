94b4c94375cb6763128fc7bcbcf3d9eb8e6599c4
Pazzurro <legaldamian133@gmail.com>
2022-10-12 11:37:35 +0200

    jedno zapytanie

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    
+    </head>
+    <body>
+        
+        <?php
+            
+            $db = new mysqli("51.123.43.245", "admin", "123456", "quiz");
+            
+            $sqlOne = "SELECT students.name, students.last_name, test.date FROM students_has_test JOIN students ON students.id = students_has_test.students_id";
+        
+        
+            //WYPISANIE PIERWSZEGO ZAPYTANIA
+            if($res = $db->query($sqlOne))
+            {
+                while($row = $res->fetch_array())
+                {
+                    echo '
+                        <p>' .$row["name"] $row["last_name"] $row["date"]. '</p>
+                    ';
+                }
+            }
+        
+            
+            $db->close();
+        ?>
+        
+    </body>
+</html>
